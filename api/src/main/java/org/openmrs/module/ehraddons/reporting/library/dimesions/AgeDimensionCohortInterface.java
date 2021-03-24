e42211714f0616e9133acd4a35aabdfa8df8b924
Thomas Juma <thomasbshop@users.noreply.github.com>
2021-03-24 10:38:54 +0300

    Update AgeDimensionCohortInterface.java

--- api/src/main/java/org/openmrs/module/ehraddons/reporting/library/dimesions/AgeDimensionCohortInterface.java
+++ api/src/main/java/org/openmrs/module/ehraddons/reporting/library/dimesions/AgeDimensionCohortInterface.java
@@ -6,6 +6,7 @@ import org.openmrs.module.reporting.evaluation.parameter.Mapped;
 import org.openmrs.module.reporting.evaluation.parameter.Parameter;
 
 /** Represents a strategy for creating cohorts for the age dimension. */
+@Component
 public interface AgeDimensionCohortInterface {
 	
 	Mapped<CohortDefinition> createXtoYAgeCohort(String name, Integer minAge, Integer maxAge);
